d7003880dc4206fc6acd8c3a43d8cfe13476c8b9
thomas garrel <dev.garrel@gmail.com>
2024-08-26 10:54:28 +0200

    test commit

--- error.vue
+++ error.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mt-7 max-w-sm mx-auto text-center card">
         <p class="mt-7 text-7xl font-bold">
-            {{  error.statusCode }}
+           Error : {{  error.statusCode }}
         </p>
         <p class="mt-7 text-5xl">
             {{  error.message }}
